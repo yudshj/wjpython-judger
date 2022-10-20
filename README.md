b49a33b8fcd0041b9d31f72c4b9a6d7075d9faa9
MagHSK <maghsk2017@gmail.com>
2022-10-20 19:34:53 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Usage
+
+`./gen_tar.sh hw1 hw2 <code_dir_names> ...`
--- gen_tar.sh
+++ gen_tar.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+
 # This script generates a tar file of the current directory.
+# Copy 选课名单.xls to the current directory.
+# The header of the xls file is: 学号	姓名	性别	系所名称	专业名称	年级	教学班号	学生来源	中期退课状态
 # Place code directory which was extracted from the tar ball in `./playground`\
 # and run this script from the root directory of the project.
 # Command: `./gen_tar.sh hw1 hw2 12345 <code_dir_name> ...`
+
 name="result"
 files=""
 args="$@"
+
 for i in $args
 do
     python3 ./judger.py $i
     files="$files playground/${i}_copied_code ${i}_result.csv"
     name="$name-$i"
 done
+
 name="${name}.tar.gz"
 tar czf $name $files
+
 echo "Generated $name"
